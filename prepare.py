d72e2bed1109eef0fd8e75a49bd62980cc3acca6
Tim Fielding <gnidleif@gmail.com>
2023-01-19 16:14:55 +0100

    finalized

---
+++ prepare.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import pathlib, os, warnings, json, uuid, shutil, imgbbpy, re
+from PIL import Image
+__location__ = pathlib.Path(__file__).parent.resolve()
+__source__ = __location__ / "source_images"
+__resized__ = __location__ / "resized_images"
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+def resize_image(image_path: str, quality: int = 90, width: int = 400, height: int = 300) -> pathlib.Path:
+    img = Image.open(image_path)
+
+    img = img.resize((width, height), Image.ANTIALIAS)
+    file_name, _ = os.path.splitext(image_path)
+    new_path = __resized__ / f"{os.path.basename(file_name)}.jpg"
+
+    img.save(new_path, quality=quality, optimize=True)
+
+    return new_path
+
+def run(_: list[str]) -> None:
+    if os.path.exists(__resized__):
+        shutil.rmtree(__resized__)
+    __resized__.mkdir(parents=True, exist_ok=True)
+
+    with open(__location__ / "config.json", 'r', encoding="utf-8") as r_json:
+        cfg = json.loads(r_json.read())
+
+    for file in pathlib.Path(__source__).glob('*.jpg'):
+        _ = resize_image(file)
+
+    # with open(__location__ / "jokes.txt", 'r', encoding="utf-8") as r_txt:
+    #     cfg["jokes"] = r_txt.read().split('\n')
+
+    img_urls = []
+    client = imgbbpy.SyncClient(cfg["api_key"])
+    for file in pathlib.Path(__resized__).glob("*.jpg"):
+        rgx = re.compile(r"https://[\w\.]+/([\w+\/-]+)")
+        url = rgx.match(client.upload(file=file, name=uuid.uuid4()).url).group(1)
+
+        img_urls.append(url)
+    cfg["image_urls"] = img_urls
+
+    with open(__location__ / "config.json", 'w', encoding='utf-8') as w_json:
+        w_json.write(json.dumps(cfg, indent=4))
+
+    with open(__location__ / "template.py", 'r', encoding="utf-8") as r_py:
+        formatted = r_py.read().format(json.dumps(cfg, indent=4))
+    
+    with open(__location__ / "chuck_norris.py", 'w', encoding="utf-8") as w_py:
+        w_py.write(formatted)
+
+    shutil.rmtree(__resized__)
+
+if __name__ == "__main__":
+    import sys
+    run(sys.argv[1:])
---
+++ template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import urllib.request, random, textwrap, certifi, imgbbpy, uuid, pathlib, os, warnings
+from io import BytesIO
+from PIL import Image, ImageFont, ImageDraw
+__location__ = pathlib.Path(__file__).parent.resolve()
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+__config__ = {0}
+
+def create_image(joke: str, img_data: str) -> Image:
+    img = Image.open(BytesIO(bytes.fromhex(img_data)))
+    font = ImageFont.truetype("impact.ttf", size=25)
+    (black, white) = ((0, 0, 0), (255, 255, 255))
+
+    (m, o) = (5, 40)
+    draw = ImageDraw.Draw(img)
+    wrapped = textwrap.wrap(joke, width = 35)
+    (_, h) = img.size
+    pos = (10, h - (len(wrapped) * o))
+
+    for i in range(len(wrapped)):
+        (x, y) = (pos[0], pos[1] + o * i)
+        (l, t, r, b) = draw.textbbox((x, y), wrapped[i], font=font)
+        draw.rectangle((l - m, t - m, r + m, b + m), fill = black)
+        draw.text((x, y), wrapped[i], font=font, fill = white)
+    
+    return img
+
+def get_resp(url: str) -> str:
+    req = urllib.request.Request(url)
+    return urllib.request.urlopen(req, cafile=certifi.where()).read()
+
+def run(_: list[str]) -> None:
+    random.seed()
+    jokes = get_resp("https://raw.githubusercontent.com/Gnidleif/chuck_norris/main/jokes.txt").splitlines()
+    joke = str(random.choice(jokes), "utf-8")
+
+    url = random.choice(__config__["image_urls"])
+    url = "https://i.ibb.co/" + url +".jpg"
+    resp = get_resp(url)
+
+    img = create_image(joke, resp.hex())
+    result = __location__ / f"result.jpg"
+    img.save(result)
+
+    client = imgbbpy.SyncClient(__config__["api_key"])
+    url = client.upload(file=result, name=uuid.uuid4()).url
+    os.remove(result)
+
+    print("Chuck Norris image generated here: " + url)
+
+if __name__ == "__main__":
+    import sys
+    run(sys.argv[1:])
